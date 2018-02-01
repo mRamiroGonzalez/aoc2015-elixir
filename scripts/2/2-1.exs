4785e2e702b48d2d6194d31070995e02d05f5082
Maxime Ramiro <mramiro@altissia.com>
2018-02-01 14:50:39 +0100

    clean step 2-1

--- scripts/2/2-1.exs
+++ scripts/2/2-1.exs
@@ -5,19 +5,33 @@ defmodule Aoc do
     def parse_input(_, surface \\ 0)
     def parse_input([], surface), do: IO.puts surface
     def parse_input([head | tail], surface) do
-        
-        dimensions = String.split(head,"x")
-        {length, _} = Integer.parse(Enum.at(dimensions,0))
-        {width, _} = Integer.parse(Enum.at(dimensions,1))
-        {height, _} = Integer.parse(Enum.at(dimensions,2))
-        
-        surface1 = 2*length*width
-        surface2 = 2*width*height
-        surface3 = 2*height*length
-        surface4 = Integer.floor_div(find_lowest(surface1, surface2, surface3),2)
 
-        increasedSurface = (surface + surface1 + surface2 + surface3 + surface4)
+        increasedSurface =
+            head
+            |> split_dimensions
+            |> get_surfaces
+            |> add(surface)
+    
         parse_input(tail, increasedSurface)
+    end
+
+    def split_dimensions(dimensions) do
+        dimensions = String.split(dimensions,"x")
+
+        {l, _} = Integer.parse(Enum.at(dimensions,0))
+        {w, _} = Integer.parse(Enum.at(dimensions,1))
+        {h, _} = Integer.parse(Enum.at(dimensions,2))
+
+        {l, w, h}
+    end
+
+    def get_surfaces({length, width, height}) do
+        surface1 = 2 * length * width
+        surface2 = 2 * height * width
+        surface3 = 2 * height * length
+        surface4 = Integer.floor_div(find_lowest(surface1, surface2, surface3), 2)
+        
+        surface1 + surface2 + surface3 + surface4
     end
 
     def find_lowest(s1, s2, s3) do
@@ -31,6 +45,9 @@ defmodule Aoc do
             end
         end
     end
+    
+    def add(a, b), do: a+b
+
 end
 
 {:ok, input} = File.read("2-input.txt")
