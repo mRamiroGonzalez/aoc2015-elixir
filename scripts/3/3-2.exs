329407643b9305ad12c2372a0fc763b79d292e79
mRamiroGonzalez <maxime.ramiro@hotmail.com>
2018-02-02 14:19:25 +0100

    improve part 3-2

--- scripts/3/3-2.exs
+++ scripts/3/3-2.exs
@@ -2,23 +2,23 @@
 # AOC 2015
 
 defmodule Aoc do
+
+    @directions_map %{60 => -1, 62 => 1, 94 => 1, 118 => -1}
+
     def parseInput(_, x1\\0, y1\\0, x2\\0, y2\\0, points\\[[0,0]])
     def parseInput([], _, _, _, _, points), do: IO.puts length(Enum.uniq(points))
-    def parseInput([s,r|t], x1, y1, x2, y2, points) do
-        map = %{60 => -1, 62 => 1, 94 => 1, 118 => -1}
-        
-        if (s in [60,62]) do
-            x1 = x1 + map[s]
-        else
-            y1 = y1 + map[s]
-        end
-        if (r in [60,62]) do
-            x2 = x2 + map[r]
-        else
-            y2 = y2 + map[r]
-        end
-        
+    def parseInput([s,r|t], x1, y1, x2, y2, points) do        
+        {x1, y1} = processDirections(s, x1, y1)
+        {x2, y2} = processDirections(r, x2, y2)
         parseInput(t, x1, y1, x2, y2, points ++ [[x1,y1],[x2,y2]])
+    end
+
+    def processDirections(dir, x, y) do
+        if (dir in [60,62]) do
+            {x + @directions_map[dir], y}
+        else
+            {x, y + @directions_map[dir]}
+        end
     end
 end
 
