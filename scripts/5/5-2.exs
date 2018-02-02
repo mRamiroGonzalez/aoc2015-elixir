9b9dd2d7d76656f170e25d1a327d62d494a057fb
mRamiroGonzalez <maxime.ramiro@hotmail.com>
2018-02-02 19:01:17 +0100

    add step 5-2

--- scripts/5/5-2.exs
+++ scripts/5/5-2.exs
@@ -1,13 +1,18 @@
 
-# AOC 2015 5-1
+# AOC 2015 5-2
 
 defmodule Aoc do
 
-    def parseInput([], acc), do: IO.puts acc    
-    def parseInput([h|t], acc) do
-        
+    @rule1 ~r/([a-zA-Z]{2}).*\1/
+    @rule2 ~r/([a-zA-Z]).\1/
+
+    def parseInput(list) do
+        nb = Enum.count list, fn e ->
+            Regex.match?(@rule1, e) && Regex.match?(@rule2, e)
+        end
+        IO.puts nb
     end
 end
 
 {:ok, file} = File.read("5-input.txt")
-String.split(file, "\n") |> Aoc.parseInput(0)
+String.split(file, "\n") |> Aoc.parseInput()
