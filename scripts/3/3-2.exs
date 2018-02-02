b91e0258e777d3854fe70b66f15a576b4aaafa3d
mRamiroGonzalez <maxime.ramiro@hotmail.com>
2018-02-02 14:12:01 +0100

    improve part 3-2

--- scripts/3/3-2.exs
+++ scripts/3/3-2.exs
@@ -2,59 +2,22 @@
 # AOC 2015
 
 defmodule Aoc do
-    def parseInput(_, x1\\0, y1\\0, x2\\0, y2\\0, points \\ [[0,0]])
+    def parseInput(_, x1\\0, y1\\0, x2\\0, y2\\0, points\\[[0,0]])
     def parseInput([], _, _, _, _, points), do: IO.puts length(Enum.uniq(points))
     def parseInput([s,r|t], x1, y1, x2, y2, points) do
-        case {s,r} do
-            {60, 60} ->
-                x1 = x1 - 1
-                x2 = x2 - 1
-            {60, 94} ->
-                x1 = x1 - 1
-                y2 = y2 + 1
-            {60, 62} ->
-                x1 = x1 - 1
-                x2 = x2 + 1
-            {60, 118} ->
-                x1 = x1 - 1
-                y2 = y2 - 1
-            {94, 94} ->
-                y1 = y1 + 1
-                y2 = y2 + 1
-            {94, 60} ->
-                x2 = x2 - 1
-                y1 = y1 + 1
-            {94, 62} ->
-                x2 = x2 + 1
-                y1 = y1 + 1
-            {94, 118} ->
-                y1 = y1 + 1
-                y2 = y2 - 1            
-            {62, 62} ->
-                x1 = x1 + 1
-                x2 = x2 + 1
-            {62, 94} ->
-                x1 = x1 + 1
-                y2 = y2 + 1
-            {62, 60} ->
-                x1 = x1 + 1
-                x2 = x2 - 1
-            {62, 118} ->
-                x1 = x1 + 1
-                y2 = y2 - 1            
-            {118, 118} ->
-                y1 = y1 - 1
-                y2 = y2 - 1        
-            {118, 60} ->
-                x2 = x2 - 1
-                y1 = y1 - 1
-            {118, 94} ->
-                y1 = y1 - 1
-                y2 = y2 + 1          
-            {118, 62} ->
-                x2 = x2 + 1
-                y1 = y1 - 1
+        map = %{60 => -1, 62 => 1, 94 => 1, 118 => -1}
+        
+        if (s in [60,62]) do
+            x1 = x1 + map[s]
+        else
+            y1 = y1 + map[s]
         end
+        if (r in [60,62]) do
+            x2 = x2 + map[r]
+        else
+            y2 = y2 + map[r]
+        end
+        
         parseInput(t, x1, y1, x2, y2, points ++ [[x1,y1],[x2,y2]])
     end
 end
