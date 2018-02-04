42a89dfd0b75bacbb3205994f01cb263346180d1
mRamiroGonzalez <maxime.ramiro@hotmail.com>
2018-02-04 14:55:28 +0100

    clean step 6-2

--- scripts/6/6-2.exs
+++ scripts/6/6-2.exs
@@ -1,14 +1,11 @@
 
 # AOC 2015 6-2
 
-# 543903
-
 defmodule Aoc do
 
     def parse_input(list, 0) do
         line = Enum.reduce 0..999, %{}, fn x, acc -> Map.put(acc, x, 0) end
         grid = Enum.reduce 0..999, %{}, fn x, acc -> Map.put(acc, x, line) end
-
         parse_input(list, grid)
     end
     def parse_input([h|t], grid) do
@@ -29,56 +26,31 @@ defmodule Aoc do
         IO.puts "Do:#{i} From:(#{x1},#{y1}) To:(#{x2},#{y2})"  
         case i do
             "toggle" ->
-                a = Enum.reduce 0..999, %{}, fn y, acc -> Map.put(acc, y, nil) end
-                b = Enum.reduce 0..999, %{}, fn x, acc -> Map.put(acc, x, a) end
-                c = Enum.reduce y1..y2, %{}, fn y, acc -> Map.put(acc, y, 2) end
-                d = Enum.reduce x1..x2, %{}, fn x, acc -> Map.put(acc, x, c) end
-                onMap = Map.merge(b, d)
-                Map.merge(grid, onMap, fn _, v1, v2 -> 
-                    Map.merge(v1, v2, fn _, vv1, vv2 ->
-                        if(vv2 == nil) do
-                            vv1
-                        else
-                            vv1 + vv2
-                        end
-                    end) 
-                end)
+                add_to_tab(grid, x1, y1, x2, y2, 2)
             "on" ->
-                a = Enum.reduce 0..999, %{}, fn y, acc -> Map.put(acc, y, nil) end
-                b = Enum.reduce 0..999, %{}, fn x, acc -> Map.put(acc, x, a) end
-                c = Enum.reduce y1..y2, %{}, fn y, acc -> Map.put(acc, y, 1) end
-                d = Enum.reduce x1..x2, %{}, fn x, acc -> Map.put(acc, x, c) end
-                onMap = Map.merge(b, d)
-                Map.merge(grid, onMap, fn _, v1, v2 -> 
-                    Map.merge(v1, v2, fn _, vv1, vv2 ->
-                        if(vv2 == nil) do
-                            vv1
-                        else
-                            vv1 + vv2
-                        end
-                    end) 
-                end)
+                add_to_tab(grid, x1, y1, x2, y2, 1)
             "off" ->
-                a = Enum.reduce 0..999, %{}, fn y, acc -> Map.put(acc, y, nil) end
-                b = Enum.reduce 0..999, %{}, fn x, acc -> Map.put(acc, x, a) end
-                c = Enum.reduce y1..y2, %{}, fn y, acc -> Map.put(acc, y, -1) end
-                d = Enum.reduce x1..x2, %{}, fn x, acc -> Map.put(acc, x, c) end
-                offMap = Map.merge(b, d)
-                Map.merge(grid, offMap, fn _, v1, v2 -> 
-                    Map.merge(v1, v2, fn _, vv1, vv2 ->
-                        if(vv2 == nil) do
-                            vv1
-                        else
-                            if (vv1 > 0) do
-                                vv1 + vv2
-                            else
-                               vv1 
-                            end
-                        end
-                    end) 
-                end)
+                add_to_tab(grid, x1, y1, x2, y2, -1)
         end
     end
+
+    def add_to_tab(grid, x1, y1, x2, y2, value) do
+        baseLine = Enum.reduce 0..999, %{}, fn y, acc -> Map.put(acc, y, nil) end
+        baseGrid = Enum.reduce 0..999, %{}, fn x, acc -> Map.put(acc, x, baseLine) end
+        partialLine = Enum.reduce y1..y2, %{}, fn y, acc -> Map.put(acc, y, value) end
+        partialGrid = Enum.reduce x1..x2, %{}, fn x, acc -> Map.put(acc, x, partialLine) end
+        mergeMap = Map.merge(baseGrid, partialGrid)
+
+        Map.merge grid, mergeMap, fn _, v1, v2 -> 
+            Map.merge v1, v2, fn _, vv1, vv2 ->
+                merge(vv1, vv2)
+            end
+        end
+    end
+
+    def merge(a, nil), do: a
+    def merge(a, b) when a + b < 0, do: a
+    def merge(a, b), do: a + b
 
     def get_instructions(s) do
         {i, firstPos, secondPos} = split_instruction(s)
@@ -108,8 +80,6 @@ defmodule Aoc do
     end
 end
 
-IO.puts "#{inspect :calendar.universal_time()}"
 File.read!("6-input.txt") 
 |> String.split("\n") 
 |> Aoc.parse_input(0)
-IO.puts "#{inspect :calendar.universal_time()}"
