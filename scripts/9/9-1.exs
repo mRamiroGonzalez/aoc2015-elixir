56c93e39222df4c5224ed816e4eefccc19c117bd
Maxime Ramiro <mramiro@altissia.com>
2018-02-08 22:49:06 +0100

    continue 9-2, only cost left

--- scripts/9/9-1.exs
+++ scripts/9/9-1.exs
@@ -7,7 +7,6 @@ defmodule InstructionSplitter do
         %{from: Enum.at(a,0), to: Enum.at(a,2), dist: Enum.at(a,4)}
     end
 
-
     def getCitiesList(instuctions, cities \\ []) do
         Enum.reduce instuctions, [], fn (x, acc) ->
              if(x.from not in acc) do
@@ -18,40 +17,68 @@ defmodule InstructionSplitter do
                  end 
              end
         end
-     end    
+     end
+     
+     def getPathsList(lines) do
+        instructions = String.split(lines, "\n")
+        Enum.reduce instructions, [], fn (x, acc) ->
+            line =  String.replace(x, "\"", "")
+                |>  String.replace("[", "")
+                |>  String.replace("]", "")
+                |>  String.split(", ")
+                |>  Enum.reduce([], fn(y, acc2) -> acc2 ++ [y] end)
+
+            acc ++ [line]
+        end
+     end
 end
 
 defmodule PathFinder do
-    def get_all_paths_for(cities) do
+    def getAllPathsFor(cities) do
         Enum.each cities, fn(x) ->
-            path_from(cities, x)
+            pathFrom(cities, x)
         end
     end
 
-    def path_from(cities, current, visited \\ []) do
+    def pathFrom(cities, current, visited \\ []) do
         visited = visited ++ [current]
 
         Enum.each cities, fn(x) ->
             if (not x in visited) do
-                path_from(cities, x, visited)                
+                pathFrom(cities, x, visited)                
             end
         end
 
         if (length(visited) == length(cities)) do
-            IO.inspect visited    
+            # IO.inspect visited
+            # {:ok, file} = File.open("paths.txt",[:append])
+            # IO.write(file, inspect visited)
+            # IO.write(file, "\n")
+            # File.close(file)
         end
     end    
     
-    def get_cost(costs, from, to) do
+end
+
+defmodule CostCalculator do
+    alias InstructionSplitter
+    
+    def getMinCost(costs) do
+        paths = InstructionSplitter.getPathsList(File.read!("paths.txt"))
+        IO.inspect paths
+        IO.inspect costs
+    end
+
+    def getCost(costs, from, to) do
         res = Enum.find costs, fn(x) -> (x.city1 == from or x.city1 == to) and (x.city2 == from or x.city2 == to) end
         res.cost
     end
-
 end
 
 defmodule Aoc do
     alias InstructionSplitter
     alias PathFinder
+    alias CostCalculator
 
     def parseInput(lines, tab \\ [])
     def parseInput([h|t], tab) do
@@ -64,7 +91,8 @@ defmodule Aoc do
     end
 
     def processInstructions(tab, cities) do
-        PathFinder.get_all_paths_for(cities)
+        PathFinder.getAllPathsFor(cities)
+        CostCalculator.getMinCost(tab)
     end
 end
 
