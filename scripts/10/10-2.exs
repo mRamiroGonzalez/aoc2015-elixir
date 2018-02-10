eb5419f4c6cf4415f19f087ed0fa950911ff12e3
Maxime Ramiro <mramiro@altissia.com>
2018-02-10 16:27:27 +0100

    clean step 10-2

--- scripts/10/10-2.exs
+++ scripts/10/10-2.exs
@@ -10,13 +10,11 @@ defmodule Aoc do
     end
 
     def countOneStep(list, acc \\ 0)
-    def countOneStep(list, 50), do: IO.puts "#{length(list)}"
-    def countOneStep([n], acc), do: countOneStep([1] ++ [n], acc + 1)
-    def countOneStep([h|t], acc) do
+    def countOneStep(list, 50), do: IO.puts "Final length: #{length(list)}"
+    def countOneStep(list, acc) do
         start = :os.system_time(:millisecond)
 
-        currentList = [h] ++ t
-        newlist = processCurrentStep(currentList)
+        newlist = processCurrentStep(list)
 
         stop = :os.system_time(:millisecond)
         IO.puts "Step #{acc + 1} took #{(stop - start) / 1000} seconds"
@@ -36,7 +34,7 @@ defmodule Aoc do
 
     def findNbFirstChar([a, a, a | _]), do: 3
     def findNbFirstChar([a, a | _]), do: 2 
-    def findNbFirstChar([a | _]), do: 1
+    def findNbFirstChar([_ | _]), do: 1
 end
 
 seed = 1321131112
