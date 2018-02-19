112693465704fea30c6cdc176468318c094d4139
Maxime Ramiro <mramiro@altissia.com>
2018-02-19 21:58:49 +0100

    add step 15-1

--- lib/15/15-1.ex
+++ lib/15/15-1.ex
@@ -1,7 +1,79 @@
 
 defmodule Aoc2015.Fifteen.One do
 
+  @filePath "lib/15/recipes.txt"
+
+  def parseInput(lines) do
+    Enum.reduce(lines, %{}, fn (line, acc) ->
+      words = String.split(line, " ")
+      ingredient =
+        %{}
+        |> Map.put(:capacity, String.to_integer(Enum.at(words, 2)))
+        |> Map.put(:durability, String.to_integer(Enum.at(words, 4)))
+        |> Map.put(:flavor, String.to_integer(Enum.at(words, 6)))
+        |> Map.put(:texture, String.to_integer(Enum.at(words, 8)))
+        |> Map.put(:calories, String.to_integer(Enum.at(words, 8)))
+      Map.put(acc, String.to_atom(String.downcase(Enum.at(words, 0))), ingredient)
+    end)
+  end
+
+  def getScoresFromFile() do
+    numbersAsStrings = File.read!(@filePath) |> String.split("\n")
+
+    numbers = Enum.reduce numbersAsStrings, [], fn (x, acc) ->
+      if (x != "") do
+        [String.to_integer(x) | acc]
+      else
+        acc
+      end
+    end
+
+    IO.inspect(Enum.max(numbers))
+  end
+
+  def createRecipes(instructions, limit, score \\ 0)do
+    Enum.each 0..limit, fn a ->
+      Enum.each 0..limit, fn b ->
+        Enum.each 0..limit, fn c ->
+          Enum.each 0..limit, fn d ->
+            if(a + b + c + d == limit) do
+              saveToFile getRecipeScore([a,b,c,d], instructions)
+            end
+          end
+        end
+      end
+    end
+  end
+
+  def getRecipeScore(recipe, instructions) do
+    scoreCapacity = getScoreFor(instructions, recipe, :capacity)
+    scoreDurability = getScoreFor(instructions, recipe, :durability)
+    scoreFlavor = getScoreFor(instructions, recipe, :flavor)
+    scoreTexture = getScoreFor(instructions, recipe, :texture)
+
+    scoreCapacity * scoreDurability * scoreFlavor * scoreTexture
+  end
+
+  def getScoreFor(instructions, [frosting, peanutButter, sprinkles, sugar], stat) do
+    score = (instructions.frosting[stat] * frosting) + (instructions.peanutbutter[stat] * peanutButter) + (instructions.sprinkles[stat] * sprinkles) + (instructions.sugar[stat] * sugar)
+    if (score < 0), do: 0, else: score
+  end
+
+  def saveToFile(elem) do
+    File.open!(@filePath, [:append])
+    |> IO.write((inspect elem) <> "\n" )
+    |> File.close
+  end
+
   def start do
-    IO.puts "15-1"
+    File.rm(@filePath)
+    File.read!("lib/15/15-input.txt")
+    |> String.replace(":","")
+    |> String.replace(",","")
+    |> String.split("\n")
+    |> parseInput
+    |> createRecipes(100)
+    getScoresFromFile
+    File.rm(@filePath)
   end
 end
